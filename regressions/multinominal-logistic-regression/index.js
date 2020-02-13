ab7e6abdc3e134f0d864c982c2a2451c8eb2dc9d
Adam Friedl <adam.friedl@rakuten.com>
2020-02-13 09:46:52 -0800

    update regression params

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 const mnist = require('mnist-data');
 
 function loadData() {
-  const mnistData = mnist.training(0, 20000);
+  const mnistData = mnist.training(0, 60000);
 
   const features = mnistData.images.values.map(image => _.flatMap(image));
   const encodedLabels = mnistData.labels.values.map(label => {
@@ -22,13 +22,13 @@ const { features, labels } = loadData();
 
 const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
-  iterations: 5,
-  batchSize: 100,
+  iterations: 40,
+  batchSize: 500,
 });
 
 regression.train();
 
-const testMnistData = mnist.testing(0, 1000);
+const testMnistData = mnist.testing(0, 10000);
 const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
 const testEncodedLabels = testMnistData.labels.values.map(label => {
   const row = new Array(10).fill(0);
@@ -38,3 +38,7 @@ const testEncodedLabels = testMnistData.labels.values.map(label => {
 
 const accuracy = regression.test(testFeatures, testEncodedLabels);
 console.log(`Accuracy is ${accuracy}`);
+
+plot({
+  x: regression.costHistory.reverse(),
+});
